658cb052244258d03db6957c10611d9a4c103e61
Sm0keF1nger <kkaptelov@gmail.com>
2022-11-23 22:42:45 +0200

    first commit

---
+++ lab4_z4/lab4_z4.c
@@ -0,0 +1,17 @@
+﻿#include <stdio.h>
+#include <math.h>
+
+int main()
+{
+
+    double y, x, dx = 0.25, a = -3, b = 6;
+    x = a;
+    do {
+        y = pow(x, 3);
+        printf_s("______________________________\n| x = %.2lf | f(x) = %.6lf |\n", x, y);
+        x += dx;
+        
+    } while (x <= b);
+}
+
+
